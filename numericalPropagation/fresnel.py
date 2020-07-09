b1db93acf2be9c7a01755e6efc5afe8ceb5210ad
Carlos Alejandro Trujillo <63428973+catrujilla@users.noreply.github.com>
2020-07-09 09:28:07 -0500

    Update fresnel.py
    minor changes

--- numericalPropagation/fresnel.py
+++ numericalPropagation/fresnel.py
@@ -77,21 +77,21 @@ def fresnel(field, z, wavelength, dx, dy):
     y = np.arange(0, M, 1)  # array y
     X, Y = np.meshgrid(x - (N / 2), y - (M / 2), indexing='xy')
 
-	dxout = (wavelength * z) / (M * dx)
-	dyout = (wavelength * z) / (N * dy)
+    dxout = (wavelength * z) / (M * dx)
+    dyout = (wavelength * z) / (N * dy)
 	
-	k =  (2 * pi ) / wavelength
+    k =  (2 * pi ) / wavelength
 	
-    z_phase = np.exp2((1j * k * z )/ (1i * wavelength * z))
-	out_phase = np.exp2((1i * pi / (wavelength * z)) * (np.power(X * dxout, 2) + np.power(Y * dyout, 2)) )
-	in_phase = np.exp2((1i * pi / (wavelength * z)) * (np.power(X * dx, 2) + np.power(Y * dy, 2)))
+    z_phase = np.exp2((1j * k * z )/ (1j * wavelength * z))
+    out_phase = np.exp2((1j * pi / (wavelength * z)) * (np.power(X * dxout, 2) + np.power(Y * dyout, 2)) )
+    in_phase = np.exp2((1j * pi / (wavelength * z)) * (np.power(X * dx, 2) + np.power(Y * dy, 2)))
 
-	tmp = (field * in_phase)
-	tmp = np.fft.fftshift(tmp)
-	tmp = np.fft.fft2(tmp)
-	tmp = np.fft.fftshift(tmp)
+    tmp = (field * in_phase)
+    tmp = np.fft.fftshift(tmp)
+    tmp = np.fft.fft2(tmp)
+    tmp = np.fft.fftshift(tmp)
 
-	out = z_phase * out_phase * dx * dy * tmp
+    out = z_phase * out_phase * dx * dy * tmp
 	
     return out
 
