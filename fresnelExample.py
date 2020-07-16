b91b58860058c4e69326e0f0374522379868e337
Carlos Alejandro Trujillo <63428973+catrujilla@users.noreply.github.com>
2020-07-16 12:02:44 -0500

    code changes

--- fresnelExample.py
+++ fresnelExample.py
@@ -22,22 +22,26 @@ import utilities
 utilities.salutation()
 
 #Load an image and automatically converts it into a gray-scale image
-hologram = utilities.imageRead('data/Holo.bmp')
+hologram = utilities.imageRead('data/HoloMedal_3.tif')
 
 #Display an gray value image with the given title
 #imageShow (hologram, 'Hologram')
 
 #This function calculates the propagated complex field of an input transmitance 
 #(in this case this function is used to numerically reconstruct an hologram)
-i = 72
-complexfield = numericalPropagation.fresnel( (hologram - np.average(hologram)), 632.8e-9, i/100, 11e-6, 11e-6)
+i = 380
+#complexfield = numericalPropagation.fresnel( (hologram - np.average(hologram)), 632.8e-9, i/100, 11e-6, 11e-6)
 
-#This function calculates the amplitude representatin of a given complex field
-#(The second parameter allows to determine if a log operation is performed)
-out = utilities.display.amplitude(complexfield, False)
+for i in range (250,350,10):
+    
+    complexfield = numericalPropagation.fresnel( (hologram - np.average(hologram)), 532, i/100, 4.5e-6, 4.5e-6)
 
-#Display an gray value image with the given title
-imageShow (out, 'Amplitude recontruction - log display ' + str(i) + ' cms' )
+    #This function calculates the amplitude representatin of a given complex field
+    #(The second parameter allows to determine if a log operation is performed)
+    out = utilities.display.amplitude(complexfield, True)
+
+    #Display an gray value image with the given title
+    imageShow (out, 'Amplitude recontruction - log display ' + str(i) + ' cms' )
 
 #Save this data into a image file in the disk
 #utilities.imageSave ('Amplitude recontruction - log display ' + str(i) + ' cms.bmp' , out)
