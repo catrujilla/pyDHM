75d916d0783d226cff2bf7a0198866273707b3f0
Carlos Alejandro Trujillo <catrujilla@eafit.edu.co>
2022-05-11 06:13:16 -0500

    Delete __init__.py
    deleting old script

--- numericalPropagation/__init__.py
+++
@@ -1,9 +0,0 @@
-# libraries
-import numpy as np
-from math import pi
-
-def info():
-    
-    print ("The available propagators are Fresnel Transform, Fresnel-Bluestein Transform and Angular spectrum")
-    
-    return
