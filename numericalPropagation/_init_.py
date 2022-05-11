2870ac9046a4fdee18c4a0c40a265668be182b73
Carlos Alejandro Trujillo <catrujilla@eafit.edu.co>
2022-05-11 06:13:50 -0500

    Create _init_.py
    init file update

---
+++ numericalPropagation/_init_.py
@@ -0,0 +1,7 @@
+# numericalPropagation package information
+def info():
+    print("The available propagators are Fresnel Transform, Fresnel-Bluestein Transform and Angular spectrum")
+
+    return
+
+
