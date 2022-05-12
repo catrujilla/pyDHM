e1eb45be2ef0984a78a5261e94d4ca552f8b4ada
Carlos Alejandro Trujillo <catrujilla@eafit.edu.co>
2022-05-12 11:01:42 -0500

    Delete _init_.py

--- numericalPropagation/_init_.py
+++
@@ -1,7 +0,0 @@
-# numericalPropagation package information
-def info():
-    print("The available propagators are Fresnel Transform, Fresnel-Bluestein Transform and Angular spectrum")
-
-    return
-
-
