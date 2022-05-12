bb5dad245b07c2fe325df83b03b4df1a9fdd349e
Carlos Alejandro Trujillo <catrujilla@eafit.edu.co>
2022-05-12 11:01:31 -0500

    Update example_numericalPropagation.py

--- example_numericalPropagation.py
+++ example_numericalPropagation.py
@@ -1,99 +1,99 @@
 """
-Title-->            Numerical propagation example.
+Title-->            Numerical propagation examples.
 Author-->           Ana Doblas, Carlos Trujillo, Raul Castaneda,
-Date-->             12/20/2021
-                    University of Memphis -  Optical Imaging Research laboratory (OIRL)
-                    EAFIT University - Applied Optics Group
+Date-->             12/05/2022
+                    University of Memphis
+                    Optical Imaging Research lab (OIRL)
+                    EAFIT University
+                    Applied Optics Group
+Abstract -->        Script that implements the different methods to propgate the resulting complex field data
+Links-->          - https://unal-optodigital.github.io/JDiffraction/
 Links-->          -
 
 """
+from pyDHM import utilities
+from pyDHM import numericalPropagation
 import numpy as np
-import utilities._init_ as ui
-import utilities.display as dis
-import numericalPropagation.propagators as pr
-import utilities.tools as tl
-import utilities.speckleMethods as speckle
 
 # Angular spectrum
 print ("Angular spectrum example")
 # Load the input plane
-input = ui.imageRead('data/numericalPropagation samples/UofM-1-inv.jpg')
-input = ui.imageRead('data/numericalPropagation samples/+3cm.tif')
-ui.imageShow(input, 'input field')
+inp = utilities.imageRead('data/numericalPropagation samples/UofM-1-inv.jpg')
+inp = utilities.imageRead('data/numericalPropagation samples/+3cm.tif')
+utilities.imageShow(inp, 'input field')
 
 # FT of the hologram
-ft_holo = dis.FT(input)
-ft_holo = dis.intensity(ft_holo, True)
-ui.imageShow(ft_holo, 'FT hologram')
+ft_holo = utilities.FT(inp)
+ft_holo = utilities.intensity(ft_holo, True)
+utilities.imageShow(ft_holo, 'FT hologram')
 
 # Spatial filter
-filter = tl.sfc(input, 160, 303, 276)
+filter = utilities.sfc(inp, 160, 303, 276)
 
 # Numerical propagation using the angular spectrum
-output = pr.angularSpectrum(filter, 70000, 0.633, 6.9, 6.9)
+output = numericalPropagation.angularSpectrum(filter, 70000, 0.633, 6.9, 6.9)
 
 # Display the output field
-intensity = dis.intensity(output, False)
-ui.imageShow(intensity, 'focused output field')
+intensity = utilities.intensity(output, False)
+utilities.imageShow(intensity, 'focused output field')
 
 for z in range(-100, 80, 10):
     # Warning: Depending on the propagation distance step, this cycle can takke too much time.
     # Numerical propagation using the angular spectrum
-    output = pr.angularSpectrum(filter, z, 0.000633, 0.0069, 0.0069)
+    output = numericalPropagation.angularSpectrum(filter, z, 0.000633, 0.0069, 0.0069)
 
     # Display the output field
-    intensity = dis.intensity(output, False)
-    ui.imageShow(intensity, 'output field ' + str(z))
+    intensity = utilities.intensity(output, False)
+    utilities.imageShow(intensity, 'output field ' + str(z))
 
 
 print ("Fresnel transform example")
 # Fresnel transform and speckle reduction vu HM2F
 
 # Load the input plane
-input = ui.imageRead('data/numericalPropagation samples/horse.bmp')
-ui.imageShow(input, 'input field')
+inp = utilities.imageRead('data/numericalPropagation samples/horse.bmp')
+utilities.imageShow(inp, 'input field')
 
 # FT of the hologram
-ft_holo = dis.FT(input)
-ft_holo = dis.intensity(ft_holo, True)
-ui.imageShow(ft_holo, 'FT hologram')
+ft_holo = utilities.FT(inp)
+ft_holo = utilities.intensity(ft_holo, True)
+utilities.imageShow(ft_holo, 'FT hologram')
 
 # Spatial filter
-filter = tl.sfr(input, 74, 500, 54, 450)
+filter = utilities.sfr(inp, 74, 500, 54, 450)
 
 # Numerical propagation using the Fresnel transforms
-output = pr.fresnel(filter, -450, 0.000633, 0.005000, 0.005000)
+output = numericalPropagation.fresnel(filter, -450, 0.000633, 0.005000, 0.005000)
 
 # Display the output field
-amplitude = dis.amplitude(output, False)
-ui.imageShow(amplitude, 'output field')
+amplitude = utilities.amplitude(output, False)
+utilities.imageShow(amplitude, 'output field')
 
 # HM2F to reduce the speckle
-denoise = speckle.HM2F(amplitude, 7, False, False)
+denoise = utilities.HM2F(amplitude, 7, False, False)
 
-#amplitude = dis.amplitude(denoise, False)
-ui.imageShow(amplitude, 'output field denoised')
+#amplitude = utilities.amplitude(denoise, False)
+utilities.imageShow(amplitude, 'output field denoised')
 
 
 # Fresnel-Bluestein transform
-
+print ("Fresnel-Bluestein transform example")
 # Load the input plane
-input = ui.imageRead('data/numericalPropagation samples/die_1.jpg')
-ui.imageShow(input, 'input field')
+inp = utilities.imageRead('data/numericalPropagation samples/die_1.jpg')
+utilities.imageShow(inp, 'input field')
 
 # FT of the hologram
-ft_holo = dis.FT(input)
-ft_holo = dis.intensity(ft_holo, True)
-ui.imageShow(ft_holo, 'FT hologram')
+ft_holo = utilities.FT(inp)
+ft_holo = utilities.intensity(ft_holo, True)
+utilities.imageShow(ft_holo, 'FT hologram')
 
 # Spatial filter
-filter = tl.sfr(input, 280, 500, 150, 340)
+filter = utilities.sfr(inp, 280, 500, 150, 340)
 
 # Numerical propagation using the Fresnel transforms
-output = pr.bluestein(filter - np.average(filter), 30/100, 632.8e-9, 7e-6, 7e-6, 4.5e-5, 4.5e-5)
+output = numericalPropagation.bluestein(filter - np.average(filter), 30/100, 632.8e-9, 7e-6, 7e-6, 4.5e-5, 4.5e-5)
 
 # Display the output field
-amplitude = dis.amplitude(output, False)
-ui.imageShow(amplitude, 'output field')
-
+amplitude = utilities.amplitude(output, False)
+utilities.imageShow(amplitude, 'output field')
 
