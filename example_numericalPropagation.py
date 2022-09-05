b1d73a334c7e79c1ac7938646b796fe57cf5e513
Carlos Alejandro Trujillo <catrujilla@eafit.edu.co>
2022-09-05 04:48:48 -0500

    Update example_numericalPropagation.py

--- example_numericalPropagation.py
+++ example_numericalPropagation.py
@@ -10,6 +10,7 @@ Abstract -->        Script that implements the different methods to propagate th
 Links-->          - https://github.com/catrujilla/pyDHM
 
 """
+
 from pyDHM import utilities
 from pyDHM import numericalPropagation
 import numpy as np
@@ -17,7 +18,7 @@ import numpy as np
 # Angular spectrum
 print ("Angular spectrum example")
 # Load the input plane
-inp = utilities.imageRead('data/numericalPropagation samples/UofM-1-inv.jpg')
+#inp = utilities.imageRead('data/numericalPropagation samples/UofM-1-inv.jpg')
 inp = utilities.imageRead('data/numericalPropagation samples/+3cm.tif')
 utilities.imageShow(inp, 'input field')
 
